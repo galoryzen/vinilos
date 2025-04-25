0247b5a5795216a57d43f4f7d4378daf8d8ddef4
David Escobar <de.escobarc1@uniandes.edu.co>
2025-04-24 21:36:49 -0500

    HU03 add fragment artist

--- app/src/main/java/com/example/vinilos/MainActivity.kt
+++ app/src/main/java/com/example/vinilos/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_album, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_album, R.id.navigation_artist
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
