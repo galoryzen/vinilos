f56a2e2c51d8bffcb407a661b506da225ce6e529
David Escobar <de.escobarc1@uniandes.edu.co>
2025-05-11 10:27:45 -0500

    HU04 add unit test

--- app/src/androidTest/java/com/example/vinilos/ArtistTest.kt
+++ app/src/androidTest/java/com/example/vinilos/ArtistTest.kt
@@ -40,4 +40,36 @@ class ArtistTest {
             withText(firstArtistName)
         ))))
     }
+
+    @Test
+    fun detailArtist() {
+        // Ir al listado de artistas
+        val artistNavigationButton = onView(withId(R.id.navigation_artist))
+        artistNavigationButton.perform(click())
+
+        Thread.sleep(waitToLoadTime)
+
+        val artistView = onView(withId(R.id.artists_recycler_view))
+
+        // Verificar que la pantalla cargue sin problemas
+        artistView.check(matches(isDisplayed()))
+
+        // Verificamos que exista al menos un artista cargado
+        artistView.check(matches(hasDescendant(withId(R.id.artist_cover_image))))
+
+        // Multiples artistas son cargados (por ende, comparten ID), obten todos y usa el primero
+        onView(allOf(withId(R.id.artist_name_text), withText(firstArtistName))).perform(
+            click()
+        )
+
+        Thread.sleep(waitToLoadTime) // Esperar que cargue la vista de detalle
+
+        // Verificar que la vista de detalle muestre el nombre del artista
+        onView(withId(R.id.detail_artist_title))
+            .check(matches(withText(firstArtistName)))
+
+        // Verificar que la descripcion y el cover estan visibiles
+        onView(withId(R.id.detail_artist_description)).check(matches(isDisplayed()))
+        onView(withId(R.id.detail_artist_cover)).check(matches(isDisplayed()))
+    }
 }
