8fb19d41fa13eaa952edebfdfca7cce3a858d13c
JuanSeromo-UniAndes <js.rodriguezm12345@uniandes.edu.co>
2025-05-11 12:00:31 -0500

    home adapter modified

--- app/src/main/java/com/example/vinilos/ui/home/HomeAdapter.kt
+++ app/src/main/java/com/example/vinilos/ui/home/HomeAdapter.kt
@@ -10,37 +10,51 @@ import com.example.vinilos.R
 import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.vinilos.data.model.Collector
+import com.example.vinilos.databinding.FragmentCollectorBinding
 import com.example.vinilos.databinding.FragmentHomeBinding
 import com.example.vinilos.ui.collector.CollectorAdapter.CollectorViewHolder
 
-class HomeAdapter(private val onItemClicked: () -> Unit) : RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
+class HomeAdapter(private val onItemClicked: (Collector) -> Unit) :
+    ListAdapter<Collector, HomeAdapter.HomeViewHolder>(CollectorsDiffCallback())
+    {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
-        val binding = FragmentHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = FragmentCollectorBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return HomeViewHolder(binding, onItemClicked)
     }
 
     inner class HomeViewHolder(
-        private val binding: FragmentHomeBinding,
-        private val onItemClicked: () -> Unit
+        private val binding: FragmentCollectorBinding,
+        private val onItemClicked: (Collector) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind() {
+        fun bind(collector: Collector) {
             itemView.setOnClickListener {
-                onItemClicked()
+                onItemClicked(collector)
             }
         }
     }
-
-
 
     /**
      * Replaces the content of an existing view with new data
      */
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        holder.bind()
+        holder.bind(getItem(position))
     }
 
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
+    }
+}
+
+class CollectorsDiffCallback : DiffUtil.ItemCallback<Collector>() {
+    override fun areItemsTheSame(oldItem: Collector, newItem: Collector): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Collector, newItem: Collector): Boolean {
+        return oldItem == newItem
     }
 }
