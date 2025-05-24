64806cd5cfd956d2262987b4f7a78198bcde95fa
Raúl López <rjlopezgr@gmail.com>
2025-05-24 09:12:27 -0500

    feat: implement LRU Cache (#45)

--- app/src/main/java/com/example/vinilos/data/repository/ArtistRepository.kt
+++ app/src/main/java/com/example/vinilos/data/repository/ArtistRepository.kt
@@ -1,6 +1,7 @@
 package com.example.vinilos.data.repository
 
 import android.util.Log
+import com.example.vinilos.data.cache.CacheManager
 import com.example.vinilos.data.model.Artist
 import com.example.vinilos.data.network.RetrofitInstance
 
@@ -9,11 +10,20 @@ class ArtistRepository {
     private val apiService = RetrofitInstance.api
 
     suspend fun getAllArtists(): List<Artist>? {
+        val cachedArtists = CacheManager.getArtistsList()
+        if (cachedArtists != null) {
+            Log.d("ArtistRepository", "Using cached artists data")
+            return cachedArtists
+        }
+
+        Log.d("ArtistRepository", "Fetching artists data from API")
         try {
             val response = apiService.getArtists()
             if (response.isSuccessful) {
-                Log.d("ArtistRepository", "Data received: ${response.body()}")
-                return response.body()
+                val artists = response.body()
+                Log.d("ArtistRepository", "Data received: $artists")
+                artists?.let { CacheManager.putArtistsList(it) }
+                return artists
             } else {
                 Log.e("ArtistRepository", "API Error Response: ${response.errorBody()?.string()}")
                 return null
@@ -25,11 +35,20 @@ class ArtistRepository {
     }
 
     suspend fun getArtistDetail(artistId: Int): Artist? {
+        val cachedArtist = CacheManager.getArtistDetail(artistId)
+        if (cachedArtist != null) {
+            Log.d("ArtistRepository", "Using cached detail data")
+            return cachedArtist
+        }
+
+        Log.d("ArtistRepository", "Fetching artist detail data from API")
         try {
             val response = apiService.getArtistDetail(artistId)
             if (response.isSuccessful) {
-                Log.d("ArtistRepository", "Detail Data received: ${response.body()}")
-                return response.body()
+                val artistDetail = response.body()
+                Log.d("ArtistRepository", "Detail Data received: $artistDetail")
+                artistDetail?.let { CacheManager.putArtistDetail(artistId, it) }
+                return artistDetail
             } else {
                 Log.e("ArtistRepository", "Detail API Error: ${response.errorBody()?.string()}")
                 return null
